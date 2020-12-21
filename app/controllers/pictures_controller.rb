974b0d5d0a6c0fa45af1b6b02eaad3dada55bd7d
misatol8k <misatolkurihara@gmail.com>
2020-12-21 22:38:46 +0900

    noticeを修正しました

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,10 +3,10 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
-  def show
-  end
   def new
     @picture = current_user.pictures.build
+  end
+  def show
   end
   def edit
   end
@@ -14,7 +14,7 @@ class PicturesController < ApplicationController
     @picture = current_user.pictures.build(picture_params)
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+        format.html { redirect_to @picture, notice: '投稿しました！' }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new }
@@ -29,7 +29,7 @@ class PicturesController < ApplicationController
   def update
     respond_to do |format|
       if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
+        format.html { redirect_to @picture, notice: 'ブログを編集しました！.' }
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit }
@@ -41,7 +41,7 @@ class PicturesController < ApplicationController
   def destroy
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+      format.html { redirect_to pictures_url, notice: '削除しました！' }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get '/pictures', to: 'pictures#index'
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users
+  resources :users, only: [:new, :create, :show]
   resources :pictures do
     collection do
       post :confirm
