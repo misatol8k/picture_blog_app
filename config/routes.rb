e2f5caca9f0a5747e5539b2f8cc1e29c9fc9b4ff
misatol8k <misatolkurihara@gmail.com>
2020-12-21 21:38:39 +0900

    picture の投稿確認機能を追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   get '/pictures', to: 'pictures#index'
-  # resources :users
-  resources :pictures
-  resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  # resources :pictures do
-  #   collection do
-  #     post :confirm
-  #   end
-  # end
+  resources :users
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
 end
