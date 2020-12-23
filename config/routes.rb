4f5ac2ec010327d1659986e2677b3765ff370f51
misatol8k <misatolkurihara@gmail.com>
2020-12-23 10:32:45 +0900

    ルーティングを整理しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  get '/pictures', to: 'pictures#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :pictures do
