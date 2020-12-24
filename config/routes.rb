8ccd3b084ad404db01888ddcf16a5b91cc1798f3
misatol8k <misatolkurihara@gmail.com>
2020-12-24 14:42:29 +0900

    ルーティングを修正しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'sessions#new'
-  get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :pictures do
